144fca0618bce940af91091c3577e079dcc23b27
Michał Pawłowski <138137627+aJpaRmp@users.noreply.github.com>
2024-09-23 19:43:29 +0200

    Delete CV directory

--- CV/JS/app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', () => {
-    const link = document.createElement('a');
-    link.href = '/CV/Curriculum vitae - Michał Pawłowski.pdf';
-    link.download = 'CV.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-});
-
