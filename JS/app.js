4628434884af5e27666e182a8a2d66233f74619e
Michał Pawłowski <138137627+aJpaRmp@users.noreply.github.com>
2024-09-23 19:44:16 +0200

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+document.getElementById("downloadBtn").addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.href = "/CV/Curriculum vitae - Michał Pawłowski.pdf";
+  link.download = "CV-MichałPawłowski.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
